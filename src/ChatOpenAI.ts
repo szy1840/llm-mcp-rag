aa7b3cfae6dbdeee1f86eb28ecfa720d3990c4c7
KelvinQiu802 <kelvinqiu802@outlook.com>
2025-04-03 23:25:49 +0800

    feat: add tool calls

--- src/ChatOpenAI.ts
+++ src/ChatOpenAI.ts
@@ -1,13 +1,23 @@
 import OpenAI from "openai";
+import { Tool } from "@modelcontextprotocol/sdk/types.js";
 import 'dotenv/config'
+
+export interface ToolCall {
+    id: string;
+    function: {
+        name: string;
+        arguments: string;
+    };
+}
 
 export default class ChatOpenAI {
     private llm: OpenAI;
     private model: string;
     private messages: OpenAI.Chat.ChatCompletionMessageParam[];
     private log: boolean;
+    private tools: Tool[];
 
-    constructor(model: string, systemPrompt?: string, log?: boolean) {
+    constructor(model: string, systemPrompt?: string, tools?: Tool[], log?: boolean) {
         this.llm = new OpenAI({
             apiKey: process.env.OPENAI_API_KEY,
             baseURL: process.env.OPENAI_BASE_URL,
@@ -15,9 +25,10 @@ export default class ChatOpenAI {
         this.model = model;
         this.messages = systemPrompt ? [{ role: "system", content: systemPrompt }] : [];
         this.log = log || false;
+        this.tools = tools || [];
     }
 
-    async chat(prompt?: string) {
+    async chat(prompt?: string): Promise<{ content: string, toolCalls: ToolCall[] }> {
         if (prompt) {
             this.messages.push({ role: "user", content: prompt });
         }
@@ -25,15 +36,37 @@ export default class ChatOpenAI {
             model: this.model,
             messages: this.messages,
             stream: true,
+            tools: this.getToolsDefinition(),
         });
-        let result = "";
+        let content = "";
+        let toolCalls: ToolCall[] = [];
         for await (const chunk of stream) {
-            const content = chunk.choices[0].delta.content || "";
-            if (this.log) process.stdout.write(content);
-            result += content;
+            const delta = chunk.choices[0].delta;
+            // 处理普通Content
+            if (delta.content) {
+                const contentChunk = chunk.choices[0].delta.content || "";
+                if (this.log) process.stdout.write(contentChunk);
+                content += contentChunk;
+            }
+            // 处理ToolCall
+            if (delta.tool_calls) {
+                for (const toolCallChunk of delta.tool_calls) {
+                    // 第一次要创建一个toolCall
+                    if (toolCalls.length <= toolCallChunk.index) {
+                        toolCalls.push({ id: toolCallChunk.index.toString(), function: { name: '', arguments: '' } });
+                    }
+                    let currentCall = toolCalls[toolCallChunk.index];
+                    if (toolCallChunk.id) currentCall.id += toolCallChunk.id;
+                    if (toolCallChunk.function?.name) currentCall.function.name += toolCallChunk.function.name;
+                    if (toolCallChunk.function?.arguments) currentCall.function.arguments += toolCallChunk.function.arguments;
+                }
+            }
         }
-        this.messages.push({ role: "assistant", content: result });
-        return result;
+        if (content !== "") this.messages.push({ role: "assistant", content: content });
+        return {
+            content: content,
+            toolCalls: toolCalls,
+        };
     }
 
     public appendToolResult(toolCallId: string, toolOutput: string) {
@@ -42,5 +75,16 @@ export default class ChatOpenAI {
             content: toolOutput,
             tool_call_id: toolCallId,
         });
+    }
+
+    private getToolsDefinition(): OpenAI.Chat.Completions.ChatCompletionTool[] {
+        return this.tools.map((tool) => ({
+            type: "function",
+            function: {
+                name: tool.name,
+                description: tool.description,
+                parameters: tool.inputSchema,
+            },
+        }));
     }
 }
