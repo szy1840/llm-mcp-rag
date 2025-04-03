189d684fcf8ca6f133be2bd5f68f0f9ac2d9205e
KelvinQiu802 <kelvinqiu802@outlook.com>
2025-04-04 00:09:01 +0800

    feat: Agent class

---
+++ src/Agent.ts
@@ -0,0 +1,59 @@
+import MCPClient from "./MCPClient";
+import ChatOpenAI from "./ChatOpenAI";
+
+export default class Agent {
+    private mcpClients: MCPClient[];
+    private llm: ChatOpenAI | null = null;
+    private model: string;
+    private systemPrompt: string;
+
+    constructor(mcpClients: MCPClient[], model: string, systemPrompt?: string) {
+        this.mcpClients = mcpClients;
+        this.model = model;
+        this.systemPrompt = systemPrompt || '';
+    }
+
+    async init() {
+        console.log('==================== TOOLS ====================');
+        for await (const client of this.mcpClients) {
+            await client.init();
+        }
+        const tools = this.mcpClients.flatMap(client => client.getTools());
+        this.llm = new ChatOpenAI(this.model, this.systemPrompt, tools);
+    }
+
+    async close() {
+        for await (const client of this.mcpClients) {
+            await client.close();
+        }
+    }
+
+    async invoke(prompt: string) {
+        if (!this.llm) throw new Error('Agent not initialized');
+        console.log('==================== CHAT ====================');
+        let response = await this.llm.chat(prompt);
+        while (true) {
+            if (response.toolCalls.length > 0) {
+                for (const toolCall of response.toolCalls) {
+                    const mcp = this.mcpClients.find(client => client.getTools().some(t => t.name === toolCall.function.name));
+                    if (mcp) {
+                        console.log('==================== TOOL USE ====================');
+                        console.log(`Calling tool: ${toolCall.function.name}`);
+                        console.log(`Arguments: ${toolCall.function.arguments}`);
+                        const result = await mcp.callTool(toolCall.function.name, JSON.parse(toolCall.function.arguments));
+                        console.log(`Result: ${JSON.stringify(result)}`);
+                        this.llm.appendToolResult(toolCall.id, JSON.stringify(result));
+                    } else {
+                        this.llm.appendToolResult(toolCall.id, 'Tool not found');
+                    }
+                }
+                // 工具调用后,继续对话
+                response = await this.llm.chat();
+                continue
+            }
+            // 没有工具调用,结束对话
+            await this.close();
+            return response.content;
+        }
+    }
+}
