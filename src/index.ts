9db12a38deb72504f83318570b29fa7e8c43bc3f
KelvinQiu802 <kelvinqiu802@outlook.com>
2025-04-03 23:25:58 +0800

    feat: add MCP client

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
+import MCPClient from "./MCPClient";
 import ChatOpenAI from "./ChatOpenAI";
 
+const URL = 'https://www.anthropic.com/engineering/building-effective-agents'
+
+const TASK = `
+Please help me summarize the content of this website.
+URL: ${URL}
+`
+
 async function main() {
-    const llm = new ChatOpenAI("gpt-4o-mini", "You are a helpful assistant.", true);
-    const result = await llm.chat("Hello, how are you?");
+    const mcp = new MCPClient("mcp-client", "uvx", ['mcp-server-fetch']);
+    await mcp.init();
+
+    const llm = new ChatOpenAI('gpt-4o-mini', TASK, mcp.getTools(), true);
+    const response = await llm.chat();
+    console.log(response.toolCalls);
+
+    await mcp.close();
 }
 
 main()
